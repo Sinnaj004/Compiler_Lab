425175d1116b57b2ae911cd3a3365272dd571da0
Jannis Steinle <jsteinle1@stud.hs-offenburg.de>
2026-04-08 10:51:15 +0200

    End of Lab1

--- src/compilers/lang_var/var_compiler.py
+++ src/compilers/lang_var/var_compiler.py
@@ -2,7 +2,8 @@ from lang_var.var_ast import *
 from common.wasm import *
 import lang_var.var_tychecker as var_tychecker
 from common.compilerSupport import *
-import common.utils as utils
+#import common.utils as utils
+
 
 def compileModule(m: mod, cfg: CompilerConfig) -> WasmModule:
     """
@@ -12,7 +13,7 @@ def compileModule(m: mod, cfg: CompilerConfig) -> WasmModule:
     vars = var_tychecker.tycheckModule(m)
     instrs = compileStmts(m.stmts)
     idMain = WasmId('$main')
-    locals: list[tuple[WasmId, WasmValtype]] = [(identToWasmId(x), 'i64') for x in vars]
+    locals: list[tuple[WasmId, WasmValtype]] = [(identToWasmId(x.name), 'i64') for x in vars]
     return WasmModule(imports=wasmImports(cfg.maxMemSize),
         exports=[WasmExport("main", WasmExportFunc(idMain))],
         globals=[],
@@ -28,7 +29,60 @@ def compileStmts(stmts: list[stmt]) -> list[WasmInstr]:
 
     for stmt in stmts:
         match stmt:
-            case StmtExp(stmt):
-                print("compiling exp stmt")
+            case StmtExp(expr):
+                instrs.extend(compileExpr(expr))
+
+                is_print = isinstance(expr, Call) and expr.name.name == 'print'
+                if not is_print:
+                    instrs.append(WasmInstrDrop())
+
+            case Assign(ident, expr):
+                instrs.extend(compileExpr(expr))
+                instrs.append(WasmInstrVarLocal('set',identToWasmId(ident.name)))
+                pass
+    return instrs
 
 
+def compileExpr(e: exp) -> list[WasmInstr]:
+    """
+    Compiles the given expression.
+    """
+    match e:
+        case IntConst(value):
+            return [WasmInstrConst('i64', value)]
+        
+        case Name(ident):
+            return [WasmInstrVarLocal('get',identToWasmId(ident.name))]
+        
+        case BinOp(left, op, right):
+            instrs = compileExpr(left)
+            instrs.extend(compileExpr(right))
+            match op:
+                case Add(): instrs.append(WasmInstrNumBinOp('i64', 'add'))
+                case Sub(): instrs.append(WasmInstrNumBinOp('i64', 'sub'))
+                case Mul(): instrs.append(WasmInstrNumBinOp('i64', 'mul'))
+            return instrs
+        
+        case UnOp(USub(), arg):
+            return [WasmInstrConst('i64', 0)] + compileExpr(arg) + [WasmInstrNumBinOp('i64', 'sub')]
+        
+        case Call(ident, args):
+            instrs: list[WasmInstr] = []
+            for arg in args:
+                instrs.extend(compileExpr(arg))
+            
+            if ident.name == 'print':
+                instrs.append(WasmInstrCall(WasmId('$print_i64')))
+            
+            elif  ident.name == 'input_int':
+                instrs.append(WasmInstrCall(WasmId('$input_i64')))
+            
+            else:
+                raise Exception(f"Unknown function: {ident.name}")
+            return instrs
+
+def identToWasmId(name: str) -> WasmId:
+    """
+    Konvertiert einen Identifier-String in eine Wasm-ID (z.B. x -> $x).
+    """
+    return WasmId(f"${name}")
