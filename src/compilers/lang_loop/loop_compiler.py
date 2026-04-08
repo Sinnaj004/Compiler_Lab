2e957b244d125968567ea446071c2d005e81f08c
Jannis Steinle <jsteinle1@stud.hs-offenburg.de>
2026-04-08 18:58:22 +0200

    IF implemented

---
+++ src/compilers/lang_loop/loop_compiler.py
@@ -0,0 +1,162 @@
+from lang_loop.loop_ast import *
+from common.wasm import *
+import lang_loop.loop_tychecker as loop_tychecker
+from common.compilerSupport import *
+from lang_loop import loop_tychecker
+#import common.utils as utils
+
+
+def compileModule(m: mod, cfg: CompilerConfig) -> WasmModule:
+    """
+    Compiles the given module.
+    """
+
+    vars = loop_tychecker.tycheckModule(m)
+    instrs = compileStmts(m.stmts)
+    idMain = WasmId('$main')
+    locals: list[tuple[WasmId, WasmValtype]] = [(identToWasmId(x.name), 'i64') for x in vars]
+    return WasmModule(imports=wasmImports(cfg.maxMemSize),
+        exports=[WasmExport("main", WasmExportFunc(idMain))],
+        globals=[],
+        data=[],
+        funcTable=WasmFuncTable([]),
+        funcs=[WasmFunc(idMain, [], None, locals, instrs)])
+
+def compileStmts(stmts: list[stmt]) -> list[WasmInstr]:
+    """
+    Compiles the given statements.
+    """
+    instrs: list[WasmInstr] = []
+
+    for stmt in stmts:
+        match stmt:
+            case StmtExp(expr):
+                instrs.extend(compileExpr(expr))
+
+                is_print = isinstance(expr, Call) and expr.name.name == 'print'
+                if not is_print:
+                    instrs.append(WasmInstrDrop())
+
+            case Assign(ident, expr):
+                instrs.extend(compileExpr(expr))
+                instrs.append(WasmInstrVarLocal('set',identToWasmId(ident.name)))
+            
+            case IfStmt(cond, thenBody, elseBody):
+                if_instrs = compileExpr(cond)
+                then_instrs = compileStmts(thenBody)
+                else_instrs = compileStmts(elseBody)
+
+                if_instrs.append(WasmInstrIf(None, then_instrs, else_instrs))
+                instrs.extend(if_instrs)
+
+            case WhileStmt(cond, body):
+                loop_instrs = compileExpr(cond)
+                body_instrs = compileStmts(body)
+
+                pass
+    return instrs
+
+
+def compileExpr(e: exp) -> list[WasmInstr]:
+    """
+    Compiles the given expression.
+    """
+    match e:
+        case IntConst(value):
+            return [WasmInstrConst('i64', value)]
+        
+        case BoolConst(value):
+            return [WasmInstrConst('i32', 1 if value else 0)]
+        
+        case Name(ident):
+            return [WasmInstrVarLocal('get',identToWasmId(ident.name))]
+        
+        case BinOp(left, op, right):
+            instrs = compileExpr(left)
+            instrs.extend(compileExpr(right))
+            match op:
+                case Add(): instrs.append(WasmInstrNumBinOp('i64', 'add'))
+                case Sub(): instrs.append(WasmInstrNumBinOp('i64', 'sub'))
+                case Mul(): instrs.append(WasmInstrNumBinOp('i64', 'mul'))
+                case Less(): instrs.append(WasmInstrIntRelOp('i32', 'lt_s'))
+                case LessEq(): instrs.append(WasmInstrIntRelOp('i32', 'le_s'))
+                case Greater(): instrs.append(WasmInstrIntRelOp('i32', 'gt_s'))
+                case GreaterEq(): instrs.append(WasmInstrIntRelOp('i32', 'ge_s'))
+                case Eq(): instrs.append(WasmInstrIntRelOp('i32', 'eq'))
+                case NotEq(): instrs.append(WasmInstrIntRelOp('i32', 'ne'))
+                case And(): pass
+                case Or(): pass
+            return instrs
+        
+        case UnOp(op, arg):
+            match op:
+                case USub():
+                    return [WasmInstrConst('i64', 0)] + compileExpr(arg) + [WasmInstrNumBinOp('i64', 'sub')]
+                case Not():
+                    return compileExpr(arg) + [WasmInstrIntRelOp('i32', 'eq')]
+        
+        case Call(ident, args):
+            instrs: list[WasmInstr] = []
+            for arg in args:
+                instrs.extend(compileExpr(arg))
+            
+            if ident.name == 'print':
+                instrs.append(WasmInstrCall(WasmId('$print_i64')))
+            
+            elif  ident.name == 'input_int':
+                instrs.append(WasmInstrCall(WasmId('$input_i64')))
+            
+            else:
+                raise Exception(f"Unknown function: {ident.name}")
+            return instrs
+
+def identToWasmId(name: str) -> WasmId:
+    """
+    Konvertiert einen Identifier-String in eine Wasm-ID (z.B. x -> $x).
+    """
+    return WasmId(f"${name}")
+
+def tyOfExp(e: exp) -> ty:
+    """
+    Gibt den Typ der gegebenen Expression zurück.
+    """
+    match e:
+        case IntConst(_):
+            return Int()
+        
+        case BoolConst(_):
+            return Bool()
+        
+        case Name(_, ty_info):
+            if isinstance (ty_info, NotVoid):
+                return ty_info.ty
+            else:
+                raise Exception(f"Variable {e.name.name} has void type")
+        
+        case BinOp(_, op, _):
+            match op:
+                case Add() | Sub() | Mul():
+                    return Int()
+                case Less() | LessEq() | Greater() | GreaterEq() | Eq() | NotEq():
+                    return Bool()
+                case And() | Or():
+                    return Bool()
+        
+        case UnOp(op, _):
+            match op:
+                case USub():
+                    return Int()
+                case Not():
+                    return Bool()
+        
+        case Call(ident, ty_info): 
+            if isinstance(ty_info, NotVoid):
+                return ty_info.ty
+            
+            if ident.name == 'input_int':
+                return Int()
+
+            if ident.name == 'print' or isinstance(ty_info, NotVoid):
+                raise Exception(f"Function {ident.name} has void type")
+            
+    raise Exception(f"Cannot determine type of expression: {e}")
