addffcce387b28595d2b2b8a28dc4ef9abed38f6
Jannis Steinle <jsteinle1@stud.hs-offenburg.de>
2026-04-23 12:53:41 +0200

    Finished Exercise1

--- src/parsers/tinyJson/tinyJson_parser.py
+++ src/parsers/tinyJson/tinyJson_parser.py
@@ -9,14 +9,13 @@ def ruleObject(toks: TokenStream) -> dict[str, Json]:
     toks.ensureNext("LBRACE")
     entryList = ruleEntryList(toks)
     toks.ensureNext("RBRACE")
-    return entryList # TODO
+    return entryList
 
 def ruleEntryList(toks: TokenStream) -> dict[str, Json]:
     if toks.lookahead().type == 'STRING':
         return ruleEntryListNotEmpty(toks)
     else:
         return {}
-    # TODO
 
 def ruleEntryListNotEmpty(toks: TokenStream) -> dict[str, Json]:
     entry = ruleEntry(toks)
@@ -27,21 +26,22 @@ def ruleEntryListNotEmpty(toks: TokenStream) -> dict[str, Json]:
         entryListNotEmpty[entry[0]] = entry[1]
         return entryListNotEmpty
     else:
-        return {entry[0]: entry[1]} # TODO
+        return {entry[0]: entry[1]} 
 
 def ruleEntry(toks: TokenStream) -> tuple[str, Json]:
-    string:str = toks.ensureNext("STRING").value
+    key = ruleString(toks)
     toks.ensureNext("COLON")
     json = ruleJson(toks)
 
-    return (string, json) # TODO
+    return (key, json) 
 
 def ruleString(toks: TokenStream) -> str:
-    return  toks.ensureNext("STRING").value # TODO
+    string = toks.ensureNext("STRING").value
+    return string[1:-1] 
 
 def ruleInt(toks: TokenStream) -> int:
     value = toks.ensureNext("INT").value
-    return value # TODO
+    return int(value) 
 
 def parse(code: str) -> Json:
     parser = mkLexer("./src/parsers/tinyJson/tinyJson_grammar.lark")
